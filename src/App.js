1613f788cf8ab56646d2ddee6b5ce4579b934ce7
Soma Hargitai <hargitai.soma@gmail.com>
2019-08-12 23:32:29 +0200

    implemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import About from "./About";
+import Shop from "./Shop";
+import ItemDetail from "./Itemdetail";
+import Nav from './Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <p>React Router introduction, following {' '}
+            <a
+              className="App-link"
+              href="https://www.youtube.com/watch?v=Law7wfdg_ls"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Dev Ed's tutorial
+            </a>
+          </p>
+        </header>
+
+        <div className="App">
+          <Nav />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/shop" exact component={Shop} />
+            <Route path="/shop/:id" component={ItemDetail} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+);
 
 export default App;
